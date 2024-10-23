0e01b057f0b531e94aa7c80fc6b9f52282f98123
Asbjrnen <asla0003@stud.kea.dk>
2024-10-23 10:07:04 +0200

    StartupPåProjekt

--- src/FilmRepetoire.java
+++ src/FilmRepetoire.java
@@ -1,2 +1,16 @@
 public class FilmRepetoire {
+
 }
+
+
+
+
+
+//As a (Role) I want (Some function) so that (some reason) ((USER STORY))
+//
+//As a user I want a program where I can create a repetoire of movies, so that I can get an overview
+//
+//
+//
+//
+//- arrayliste hvor en masse ting tilføjes imens programmet kører og fjernes igen når programmet afsluttes.
